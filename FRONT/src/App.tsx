9fa7ec1dc31f03b9bf798b4cb538f30e2f213e6d
Rafa <rgpredes@gmail.com>
2024-10-21 14:24:46 +0200

    Esqueleto de entregar nombre

--- FRONT/src/App.tsx
+++ FRONT/src/App.tsx
@@ -1,36 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nombre, setNombre] = useState<string>(''); 
+  
+  const entregar = () => {
+    alert(`Nombre entregado: ${nombre}`);
+  };
 
   return (
-    <>
-      <h1>CAMBIO SIN BUILD</h1>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="center-box">
+        <input 
+            type="text" 
+            placeholder="Introduce tu nombre" 
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            id="nombre"
+        />
+        <button onClick={entregar}>Entregar</button>
+    </div>
+);
 }
 
 export default App

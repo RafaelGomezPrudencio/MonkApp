00999c9f586f4dcfa13c4668a3b3b9893d391096
Rafa <rgpredes@gmail.com>
2024-10-21 11:12:52 +0200

    primer cambio

--- FRONT/vite.config.ts
+++ FRONT/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Especifica el puerto aquí (puedes cambiar 3000 por el número de puerto que prefieras)
+  },
   build: {
     outDir: 'dist', // Salida de la compilación
   },
